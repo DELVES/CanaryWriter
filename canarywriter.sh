e4c98e4a39c82f97af05169f9d2150abd6ef3f08
Hamzah Batha <55909543+hamzahbatha@users.noreply.github.com>
2022-03-14 22:05:16 +0000

    CanaryWriter Major Update #1
    - Switched from zsh to bash for backward compatibility.
    - File signatures are now embedded within the final signed file in cleartext.
    - Reformatted the structure of canary statements.
    - Examples of how to answer the questions when running the script are provided on-screen.

--- canarywriter.sh
+++ canarywriter.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 echo "
 █▀▀ ▄▀█ █▄░█ ▄▀█ █▀█ █▄█ █░█░█ █▀█ █ ▀█▀ █▀▀ █▀█
 █▄▄ █▀█ █░▀█ █▀█ █▀▄ ░█░ ▀▄▀▄▀ █▀▄ █ ░█░ ██▄ █▀▄"
@@ -7,14 +7,14 @@ echo "\n"
 
 sleep 1
 
-echo -n "[CW] When was the last canary statement created? `echo $'\n> '`"
+echo -n "[CW] When was the last canary statement created? (e.g 1st April 2021) `echo $'\n> '`"
 read userInput
 if [[ -n "$userInput" ]]
 then
     clu=$userInput
 fi
 
-echo -n "[CW] When will the next canary be created? `echo $'\n> '`"
+echo -n "[CW] When will the next canary be created? (e.g 1st April 2023) `echo $'\n> '`"
 read userInput
 if [[ -n "$userInput" ]]
 then
@@ -25,38 +25,27 @@ sleep 1
 
 echo "[CW] Generating canary for `date +%d-%m-%Y`..."
 cat >canary-`date +%d-%m-%Y`.txt <<EOF
+
+
+
 CANARY STATEMENT
-====================================
+===================================================
+Issued for : `date +%d` `date +%B` `date +%Y`.
 Last updated : $clu
 Next update on : $cnu
-====================================
+===================================================
 What are canary statements?
 https://en.wikipedia.org/wiki/Warrant_canary
 https://www.eff.org/deeplinks/2014/04/warrant-canary-faq
-====================================
-
-Issued for `date +%d` `date +%B` `date +%Y`.
-
+===================================================
 Don't trust this statement blindly, verify the signature of this file!
+===================================================
+
 
 Signers
 ~~~~~~~~~~~~~
 
 `cat signers.txt`
-
-
-
-
-Statements
-~~~~~~~~~~~~~
-
-`cat statement.txt`
-
-
-Special Announcements
-~~~~~~~~~~~~~~~~~~~~~~~~
-
-`cat announcements.txt`
 
 
 Proof of Freshness
@@ -76,10 +65,30 @@ Headlines from: https://www.theguardian.com/world/rss
 
 `rsstail -1 -n5 -N -u https://www.theguardian.com/world/rss`
 
+
+
+
+Statements
+~~~~~~~~~~~~~
+
+`cat statement.txt`
+
+
+Special Announcements
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+`cat announcements.txt`
+
+
+
+
 EOF
 echo "[CW] Canary generated!"
 sleep 1
 echo "[CW] Now signing..."
-gpg --clearsign canary-`date +%d-%m-%Y`.txt
+gpg --output canary-`date +%d-%m-%Y`-signed.txt --clearsign canary-`date +%d-%m-%Y`.txt
 sleep 1
-echo "[CW] Canary signed! You can now distribute canary-`date +%d-%m-%Y`.txt"
+echo "[CW] Deleting unsigned canary..."
+rm canary-`date +%d-%m-%Y`.txt
+sleep 1
+echo "[CW] Signed canary statement ready! You can now distribute canary-`date +%d-%m-%Y`-signed.txt"
